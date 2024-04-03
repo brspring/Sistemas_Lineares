7adf139223d9b60007608b2a1446a6a0d79d8190
Bruno Aziz Spring Machado <brunospring@ufpr.br>
2024-04-03 15:01:14 -0300

    bomba

--- gauss.c
+++ gauss.c
@@ -29,9 +29,9 @@ void trocaLinhas(double **A, double *b, int i, int iPivo){
 
 void eliminacaoDeGauss(double **A, double *b, uint n){
     for(int i=0; i<n; ++i){
-        uint iPivo = encontraMax(A, i, n);
-        if(i != iPivo)
-            trocaLinhas(A, b, i, iPivo);
+        //uint iPivo = encontraMax(A, i, n);
+        //if(i != iPivo)
+            //trocaLinhas(A, b, i, iPivo);
 
         for(int k=i+1; k < n; ++k){
             double m = A[k][i]/A[i][i];
@@ -44,10 +44,28 @@ void eliminacaoDeGauss(double **A, double *b, uint n){
             
     }
 }
+
+void eliminacaoDeGaussTriDiagonais(double **A, double *b, int n){
+    for(int i=0; i<n; ++i){
+        //uint iPivo = encontraMax(A, i, n);
+        //if(i != iPivo)
+            //trocaLinhas(A, b, i, iPivo);
+            int k = i+1;
+            double m = A[k][i]/A[i][i];
+            A[k][i] = 0.00;
+            int j = i+1;
+            
+            A[k][j] -= A[i][j] * m;
+            b[k] -= m * b[i];     
+            printf("valor do i = q %d\n", i); 
+            
+    }
+}
+
 int main(){
     double **Matriz;
     double *b;
-    uint n;
+    int n;
     //leitura de Matriz e b
     scanf("%d", &n);
 
@@ -62,15 +80,49 @@ int main(){
         for(int j=0; j <= n; ++j){
             if(j==n){
                 scanf("%lf", &b[i]);
+                //printf("b[%d] = %.2f\n", i, b[i]);
             }else{
                 scanf("%lf", &Matriz[i][j]);
             }
         }
     }
+    /*double *d = (double *)malloc(n * sizeof(double));
+    double *a = (double *)malloc((n-1) * sizeof(double)); 
+    double *c = (double *)malloc((n-1) * sizeof(double));*/
+    double d[n];
+    double a[n-1];
+    double c[n-1];
 
-    eliminacaoDeGauss(Matriz, b, n);
+    // vetores matriz tridiagonais 
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            if(i == j)
+                d[i] = Matriz[i][j];
+            if(i == j-1)
+                a[i] = Matriz[i][j];
+            if(i == j+1){
+                c[i] = Matriz[i][j];
+                printf("i: \n", i, c[i]);
+            }
+                
+        }
+    }
 
-    // mostra a matriz lida
+    //printando os 3 vetores 
+    for(int i=0; i<n; ++i){
+        printf("d[%d] = %.2f\n", i, d[i]);
+    }
+    for(int i=0; i<n-1; ++i){
+        printf("a[%d] = %.2f\n", i, a[i]);
+    }
+    for(int i=0; i<n-1; ++i){
+        printf("c[%d] = %.2f\n", i, c[i]);
+    }
+    //tempo = timestemp()
+    //eliminacaoDeGaussTriDiagonais(Matriz, b, n);
+    //timestemp() - tempo
+    
+    // mostra a matriz lida fazer funcao
     printf("------- Matriz resultante -------\n");
     for(int i=0; i<n; ++i){
         for(int j=0; j<=n; ++j){
