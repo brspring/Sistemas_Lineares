7eddcae8d116afd9e816ee41a9a31a64bb4a3fc1
Bruno Aziz Spring Machado <brunospring@ufpr.br>
2024-04-07 16:33:20 -0300

    meio bomba

--- scprit.sh
+++ scprit.sh
@@ -10,4 +10,5 @@ make
 # Redireciona a saída do likwid-perfctr para um arquivo
 likwid-perfctr -C ${CPU} -g ${METRICA} -m ${PROGRAMA} > saida_likwid.txt
 
+cat saida_likwid.txt | grep -A 20  "EG clássico"
 cat saida_likwid.txt | grep "DP" | grep -v "AVX"
